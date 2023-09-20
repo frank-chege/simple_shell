3be472e239af8d75c462238c263f51bf8d191706
gregorymark254 <gregorymark254@gmail.com>
2023-09-21 00:51:27 +0300

    betty

--- betty.c
+++ betty.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 /**
- * betty - program start
- * Return: 0 indicating success
+ * betty - Prints a greeting message.
+ *
+ * This function prints "Hello betty" to the standard output.
+ *
+ * Return: 0 indicating success.
  */
-
 int betty(void)
-
 {
-	printf("Hello betty\n");
-	return (0);
+    printf("Hello betty\n");
+    return (0);
 }
-
